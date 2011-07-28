826885524d543c2f14025aa2aeb2a0c2b989b020
Peter Neumark <neumark@postlock.org>
2011-07-28 14:05:44 +0200

    starting out with initial working version based on: http://userscripts.org/scripts/review/13700

---
+++ gmail_link_to_groups_message.user.js
@@ -0,0 +1,66 @@
+// ==UserScript==
+// @name           Gmail Show Details (New)
+// @namespace      http://web.mit.edu/mathmike
+// @description    Always show details in the first open message of a gmail conversation.
+// @include        http://mail.google.com/*
+// @include        https://mail.google.com/*
+// ==/UserScript==
+
+// Most of the functions below were borrowed from Gmail Macros (New)
+
+var SHOW_DETAILS_CLASS = "iD";
+
+frames.js.gmonkey.load('1.0', function(gmail) {
+var showDetails = function() {
+        var nodes = getNodesByTagNameAndClass(gmail.getActiveViewElement(), "span", SHOW_DETAILS_CLASS);
+        if (!nodes) return;
+        var show = nodes[0]; //only shows details of the first available message
+        if (!show) return;
+        if (show.innerHTML == "show details"){
+            simulateClick(show, "click");
+        }
+    },
+    simulateClick = function(node, eventType) {
+        var event = node.ownerDocument.createEvent("MouseEvents");
+        event.initMouseEvent(
+            eventType,
+            true, // can bubble
+            true, // cancellable
+            node.ownerDocument.defaultView,
+            1, // clicks
+            50, 50, // screen coordinates
+            50, 50, // client coordinates
+            false, false, false, false, // control/alt/shift/meta
+            0, // button,
+            node);
+        node.dispatchEvent(event);
+    },
+    getNodesByTagNameAndClass = function (rootNode, tagName, className) {
+        var expression = ".//" + tagName + "[contains(concat(' ', @class, ' '), ' " + className + " ')]";
+        return evalXPath(expression, rootNode);
+    },
+    evalXPath = function (expression, rootNode) {
+        try {
+            var xpathIterator = rootNode.ownerDocument.evaluate(
+                expression,
+                rootNode,
+                null, // no namespace resolver
+                XPathResult.ORDERED_NODE_ITERATOR_TYPE,
+                null); // no existing results
+        } catch (err) {
+            console.log("Error when evaluating XPath expression '" + expression + "'" + ": " + err);
+            return null;
+        }
+        var results = [];
+
+        // Convert result to JS array
+        for (var xpathNode = xpathIterator.iterateNext(); xpathNode; 
+                xpathNode = xpathIterator.iterateNext()) {
+            results.push(xpathNode);
+        }
+        return results;
+    };
+    window.setTimeout(function() {gmail.registerViewChangeCallback(showDetails);}, 500);
+});
+
+
