5ef8868451ef085efb17cec0173a2fd5df8626be
Peter Neumark <neumark@postlock.org>
2011-07-29 11:26:46 +0200

    This version displays the view in Groups link and attaches a stub eventListener.

--- gmail_link_to_groups_message.user.js
+++ gmail_link_to_groups_message.user.js
@@ -7,110 +7,111 @@
 // ==/UserScript==
 
 // from: http://erikvold.com/blog/index.cfm/2010/6/14/using-jquery-with-a-user-script
-var addJQuery = function(callback) {
-        var script = document.createElement("script");
-        script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
-        script.addEventListener('load', callback
-            /*function() {
+(function() {
+    var // getActiveViewElement() is replaced when greasemonkey object is obtained.
+        getActiveViewElement,
+        addJQuery = function(callback) {
             var script = document.createElement("script");
-            script.textContent = "(" + callback.toString() + ")();";
+            script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
+            script.addEventListener('load', callback, false);
             document.body.appendChild(script);
-        }*/, false);
-        document.body.appendChild(script);
-    },
-    action = function(gmail) {
-        var showDetails = function() {
-                var show, i,nodes = evalXPath('//img[@alt="More"]/../..', gmail.getActiveViewElement());
-                if (!nodes) return;
-                for (i = 0; i < /*nodes.length*/1; i++) {
-                    show = nodes[i]; //only shows details of the first available message
-                    if (!show) return;
-                    simulateClick(show, "click"); // open the drop-down menu.
-                    // click on the "show original" link
-                    setTimeout(showOriginal, 0);
+        },
+        addButtons = function() {
+            // find the 'Show original' link, and insert another link right after it:
+            var i, button, currSpan, nodes = evalXPath('//span[text()="show details"]', getActiveViewElement());
+            for (i = 0; i < nodes.length; i++) {
+                currSpan = jQuery(nodes[i]);
+                // if the next span already is a "google groups link" span, then do nothing:
+                if (currSpan.next().attr('google_groups_link') === 'true') {
+                    continue;
                 }
-            },
-            readurl = function(url) {
-                jQuery.ajax({
-                  url: url,
-                  success: function(data) {console.log(data.match(/Message-Id\: <(.*)>/i)[1]);},
-                  dataType: 'text'
+                // Otherwise, insert a link.
+                button = currSpan.clone();
+                button.attr('google_groups_link', 'true');
+                button.text('show in Groups');
+                button.click(function(e) {
+                    e.stopPropagation();
+                    console.log('clicked');
                 });
-            },
-            showOriginal = function() {
-                
-                var oldwindowopen = window.open,
-                    oldwindowalert = window.alert,
-                    show, i,nodes = evalXPath('//div[text()="Show original"]', gmail.getActiveViewElement());
-                window.open = function(relurl, _dc1, _dc2) {readurl(relurl);};
-                window.alert = function() {};
-                if (!nodes) return;
-                for (i = 0; i < /*nodes.length*/1; i++) {
-                    show = nodes[i]; //only shows details of the first available message
-                    if (!show) return;
-                    simulateClick(show, "mousedown"); // open the drop-down menu.
-                    simulateClick(show, "mouseup"); // open the drop-down menu.
-                }
-                window.open = oldwindowopen;
-                window.alert = oldwindowalert;
-        /*
-                console.log("show original");
-                var doxpath = function(allLinks, f) {var i = 0;for (; i < allLinks.snapshotLength; i++) {f(allLinks.snapshotItem(i));}};
-                doxpath(
-                    document.evaluate(
-                        '//div[text()="Show original"]',
-                        gmail.getActiveViewElement(),
-                        null,
-                        XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
-                        null),
-                    function(i) {console.log(i);});
-        */
-            },
-            simulateClick = function(node, eventType) {
-                var event = node.ownerDocument.createEvent("MouseEvents");
-                event.initMouseEvent(
-                    eventType,
-                    true, // can bubble
-                    true, // cancellable
-                    node.ownerDocument.defaultView,
-                    1, // clicks
-                    50, 50, // screen coordinates
-                    50, 50, // client coordinates
-                    false, false, false, false, // control/alt/shift/meta
-                    0, // button,
-                    node);
-                node.dispatchEvent(event);
-            },
-            getNodesByTagNameAndClass = function (rootNode, tagName, className) {
-                var expression = ".//" + tagName + "[contains(concat(' ', @class, ' '), ' " + className + " ')]";
-                return evalXPath(expression, rootNode);
-            },
-            evalXPath = function (expression, rootNode) {
-                try {
-                    var xpathIterator = rootNode.ownerDocument.evaluate(
-                        expression,
-                        rootNode,
-                        null, // no namespace resolver
-                        XPathResult.ORDERED_NODE_ITERATOR_TYPE,
-                        null); // no existing results
-                } catch (err) {
-                    console.log("Error when evaluating XPath expression '" + expression + "'" + ": " + err);
-                    return null;
-                }
-                var results = [];
+                currSpan.after(button);
+                button.before(jQuery('<span> </span>')); // space between links
+            }
+        },
+        viewChangeCallback = function() {
+            addButtons();
+        },
+        getRawMessage = function() {
+            var show, i,nodes = evalXPath('//img[@alt="More"]/../..', getActiveViewElement());
+            for (i = 0; i < /*nodes.length*/1; i++) {
+                show = nodes[i]; //only shows details of the first available message
+                simulateClick(show, "click"); // open the drop-down menu.
+                // click on the "show original" link
+                setTimeout(showOriginal, 0);
+            }
+        },
+        readurl = function(url) {
+            jQuery.ajax({
+              url: url,
+              success: function(data) {console.log(data.match(/Message-Id\: <(.*)>/i)[1]);},
+              dataType: 'text'
+            });
+        },
+        showOriginal = function() {
+            var oldwindowopen = window.open,
+                oldwindowalert = window.alert,
+                show, i,nodes = evalXPath('//div[text()="Show original"]', getActiveViewElement());
+            window.open = function(relurl, _dc1, _dc2) {readurl(relurl);};
+            window.alert = function() {};
+            for (i = 0; i < /*nodes.length*/1; i++) {
+                show = nodes[i]; //only shows details of the first available message
+                simulateClick(show, "mousedown"); // open the drop-down menu.
+                simulateClick(show, "mouseup"); // open the drop-down menu.
+            }
+            window.open = oldwindowopen;
+            window.alert = oldwindowalert;
+        },
+        simulateClick = function(node, eventType) {
+            var event = node.ownerDocument.createEvent("MouseEvents");
+            event.initMouseEvent(
+                eventType,
+                true, // can bubble
+                true, // cancellable
+                node.ownerDocument.defaultView,
+                1, // clicks
+                50, 50, // screen coordinates
+                50, 50, // client coordinates
+                false, false, false, false, // control/alt/shift/meta
+                0, // button,
+                node);
+            node.dispatchEvent(event);
+        },
+        evalXPath = function (expression, rootNode) {
+            var results = [];
+            try {
+                var xpathIterator = rootNode.ownerDocument.evaluate(
+                    expression,
+                    rootNode,
+                    null, // no namespace resolver
+                    XPathResult.ORDERED_NODE_ITERATOR_TYPE,
+                    null); // no existing results
+            } catch (err) {
+                console.log("Error when evaluating XPath expression '" + expression + "'" + ": " + err);
+                return results;
+            }
 
-                // Convert result to JS array
-                for (var xpathNode = xpathIterator.iterateNext(); xpathNode; 
-                        xpathNode = xpathIterator.iterateNext()) {
+            // Convert result to JS array
+            for (var xpathNode = xpathIterator.iterateNext(); xpathNode; 
+                    xpathNode = xpathIterator.iterateNext()) {
+                if (xpathNode !== null) {
                     results.push(xpathNode);
                 }
-                return results;
-            };
-            // start execution here:
-            window.setTimeout(function() {gmail.registerViewChangeCallback(showDetails);}, 500);
+            }
+            return results;
         };
-
-frames.js.gmonkey.load('1.0', function (gmail) {
-    addJQuery(function() {action(gmail);});
-});
-
+        // start execution here:
+        frames.js.gmonkey.load('1.0', function (gmObj) {
+            getActiveViewElement = function () {return gmObj.getActiveViewElement();};
+            window.setTimeout(function() {gmObj.registerViewChangeCallback(viewChangeCallback);}, 500);
+            addJQuery(viewChangeCallback);
+        });
+}());
