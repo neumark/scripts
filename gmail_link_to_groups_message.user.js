47ddba01da86b4536ec1c8e8d35e71616f85e0ee
Peter Neumark <neumark@postlock.org>
2011-08-01 09:44:35 +0200

    finished version 1.0

--- gmail_link_to_groups_message.user.js
+++ gmail_link_to_groups_message.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name           Gmail Show Details (New)
-// @namespace      http://web.mit.edu/mathmike
-// @description    Always show details in the first open message of a gmail conversation.
+// @name           Gmail open in Groups
+// @namespace      http://peterneumark.com/javascript/2011-07-29/extending-gmail-with-your-own-features/
+// @description    Allows you to open messages in Google Groups.
 // @include        http://mail.google.com/*
 // @include        https://mail.google.com/*
 // ==/UserScript==
 
-// from: http://erikvold.com/blog/index.cfm/2010/6/14/using-jquery-with-a-user-script
 (function() {
-    var // getActiveViewElement() is replaced when greasemonkey object is obtained.
-        getActiveViewElement,
+    var // getCurrentThread() is replaced when greasemonkey object is obtained.
+        getCurrentThread,
+        // from: http://erikvold.com/blog/index.cfm/2010/6/14/using-jquery-with-a-user-script
         addJQuery = function(callback) {
             var script = document.createElement("script");
             script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
@@ -18,30 +18,37 @@
         },
         addButtons = function() {
             // find the 'Show original' link, and insert another link right after it:
-            var i, button, currSpan, nodes = evalXPath('//span[text()="show details"]', getActiveViewElement());
+            var i, button, currSpan, nodes, currThread = getCurrentThread();
+            if (!currThread || !currThread.ra) return;
+            nodes = evalXPath('//span[text()="show details" and not(@google_groups_link)]', currThread.ra);
             for (i = 0; i < nodes.length; i++) {
                 currSpan = jQuery(nodes[i]);
-                // if the next span already is a "google groups link" span, then do nothing:
-                if (currSpan.next().attr('google_groups_link') === 'true') {
-                    continue;
-                }
                 // Otherwise, insert a link.
                 button = currSpan.clone();
-                button.attr('google_groups_link', 'true');
+                currSpan.attr('google_groups_link', 'true');
                 button.text('show in Groups');
                 button.click(function(e) {
+                    var cs = currSpan;
                     e.stopPropagation();
-                    console.log('clicked');
+                    // Note: class name comes from page source (may change).
+                    getRawMessage(cs.parents('div.Bk:first')[0]);
                 });
                 currSpan.after(button);
                 button.before(jQuery('<span> </span>')); // space between links
             }
         },
         viewChangeCallback = function() {
+            // add buttons whenever a new email appears
+            var currThread = getCurrentThread();
+            if (currThread && currThread.ra) {
+                jQuery('div.FL1GFc', currThread.ra).each(function() {
+                    this.addEventListener("DOMSubtreeModified", function() {addButtons();});
+                });
+            }
             addButtons();
         },
-        getRawMessage = function() {
-            var show, i,nodes = evalXPath('//img[@alt="More"]/../..', getActiveViewElement());
+        getRawMessage = function(parentNode) {
+            var show, i,nodes = evalXPath('//img[@alt="More"]/../..', parentNode);
             for (i = 0; i < /*nodes.length*/1; i++) {
                 show = nodes[i]; //only shows details of the first available message
                 simulateClick(show, "click"); // open the drop-down menu.
@@ -51,16 +58,25 @@
         },
         readurl = function(url) {
             jQuery.ajax({
-              url: url,
-              success: function(data) {console.log(data.match(/Message-Id\: <(.*)>/i)[1]);},
-              dataType: 'text'
+                url: url,
+                success: function(data) {
+                    var messageId = data.match(/Message-Id\: <(.*)>/i)[1];
+                    window.open('http://groups.google.com/groups?selm=' + messageId, "_blank", "");
+                },
+                dataType: 'text'
             });
         },
         showOriginal = function() {
             var oldwindowopen = window.open,
                 oldwindowalert = window.alert,
-                show, i,nodes = evalXPath('//div[text()="Show original"]', getActiveViewElement());
-            window.open = function(relurl, _dc1, _dc2) {readurl(relurl);};
+                currThread = getCurrentThread(),
+                show, i,nodes;
+            if (!currThread || !currThread.ra) return;
+            nodes = evalXPath('//div[text()="Show original"]', currThread.ra);
+            window.open = function(relurl, _dc1, _dc2) {
+                readurl(relurl);
+                return window;
+            };
             window.alert = function() {};
             for (i = 0; i < /*nodes.length*/1; i++) {
                 show = nodes[i]; //only shows details of the first available message
@@ -109,9 +125,14 @@
             return results;
         };
         // start execution here:
-        frames.js.gmonkey.load('1.0', function (gmObj) {
-            getActiveViewElement = function () {return gmObj.getActiveViewElement();};
-            window.setTimeout(function() {gmObj.registerViewChangeCallback(viewChangeCallback);}, 500);
+        gmonkey.load('2.0', function (gmail) {
+            getCurrentThread = function () {
+                return gmail.getCurrentThread();
+            };
+            window.setTimeout(function() {
+                gmail.registerViewChangeCallback(viewChangeCallback);
+            }, 500);
             addJQuery(viewChangeCallback);
         });
 }());
+
